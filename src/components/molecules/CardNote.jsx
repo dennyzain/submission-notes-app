dec0c6c017d1dd9e6badcf36da4ffdc9f8410a33
dennyzain <abbasdenny24@gmail.com>
2022-08-30 15:15:05 +0700

    feat: add createdAt, title, body on cardNote comp

--- src/components/molecules/CardNote.jsx
+++ src/components/molecules/CardNote.jsx
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { showFormattedDate } from '../../utils';
 
 export default function CardNote({ id, title, body, createdAt, archived }) {
+  const [state, setState] = useState(false);
+  console.log(state, 'boolean show more');
   return (
-    <>
-      <div className="w-[200px] grid grid-cols-2 grid-flow-row transition-all border-2 p-5 border-black hover:shadow-[12px_12px_0px_0px_rgba(0,0,0,1)]">
-        <div className="h-[300px]">
-          <h2 className="font-extrabold">{title}</h2>
-          <p>{body.length > 100 ? `${body.substring(100)}...` : body}</p>
-        </div>
-        <div className="space-x-2">
-          <button className="border-2 border-black px-2 transition-all hover:shadow-[12px_12px_0px_0px_rgba(0,0,0,1)]">
-            <p>Archive</p>
-          </button>
-          <button className="border-2 border-black px-2 hover:shadow-[12px_12px_0px_0px_rgba(0,0,0,1)]">
-            <p>Delete</p>
-          </button>
-        </div>
+    <div className="w-[250px] grid grid-cols-2 grid-rows-[3fr_30px_50px] transition-all border-2 py-3 px-5 border-black hover:shadow-[12px_12px_0px_0px_rgba(0,0,0,1)]">
+      <div className="h-[250px] col-span-2">
+        <h2 className="font-extrabold">{title}</h2>
+        <p>{body.length > 150 ? `${body.substring(70)}...` : body}</p>
+        <button className="font-bold" onClick={() => setState(!state)}>
+          show more
+        </button>
       </div>
-    </>
+      <div className="col-span-2">
+        <p>{showFormattedDate(createdAt)}</p>
+      </div>
+      <div className="flex col-span-2 row-start-3 items-center justify-around">
+        <button className="border-2 border-black px-2 transition-all hover:shadow-[12px_12px_0px_0px_orange]">
+          <p>Archive</p>
+        </button>
+        <button className="border-2 border-black px-2 transition-all hover:shadow-[12px_12px_0px_0px_red]">
+          <p>Delete</p>
+        </button>
+      </div>
+    </div>
   );
 }
