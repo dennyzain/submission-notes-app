742bde7c6c653e18f55c5446009bdc69fd02b1a0
dennyzain <abbasdenny24@gmail.com>
2022-08-30 16:31:03 +0700

    feat: add handle delete notes

--- src/components/molecules/CardNote.jsx
+++ src/components/molecules/CardNote.jsx
@@ -1,9 +1,18 @@
 import React, { useState } from 'react';
 import { showFormattedDate } from '../../utils';
 
-export default function CardNote({ id, title, body, createdAt, archived }) {
+export default function CardNote({
+  id,
+  title,
+  body,
+  createdAt,
+  archived,
+  handleDelete,
+  handleArchive,
+}) {
   const [state, setState] = useState(false);
   console.log(state, 'boolean show more');
+
   return (
     <div className="w-[250px] grid grid-cols-2 grid-rows-[3fr_30px_50px] transition-all border-2 py-3 px-5 border-black hover:shadow-[12px_12px_0px_0px_rgba(0,0,0,1)]">
       <div className="h-[250px] col-span-2">
@@ -17,10 +26,16 @@ export default function CardNote({ id, title, body, createdAt, archived }) {
         <p>{showFormattedDate(createdAt)}</p>
       </div>
       <div className="flex col-span-2 row-start-3 items-center justify-around">
-        <button className="border-2 border-black px-2 transition-all hover:shadow-[12px_12px_0px_0px_orange]">
+        <button
+          onClick={() => handleArchive(id)}
+          className="border-2 border-black px-2 transition-all hover:shadow-[12px_12px_0px_0px_orange]"
+        >
           <p>Archive</p>
         </button>
-        <button className="border-2 border-black px-2 transition-all hover:shadow-[12px_12px_0px_0px_red]">
+        <button
+          onClick={() => handleDelete(id)}
+          className="border-2 border-black px-2 transition-all hover:shadow-[12px_12px_0px_0px_red]"
+        >
           <p>Delete</p>
         </button>
       </div>
--- src/components/organisms/ListNotes.jsx
+++ src/components/organisms/ListNotes.jsx
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import EmptyNotes from '../atoms/EmptyNotes';
 import CardNote from '../molecules/CardNote';
 import { getInitialData } from '../../utils';
 
 export default function ListNotes() {
-  const notes = getInitialData();
+  const [notes, setNotes] = useState(getInitialData());
+  const handleDelete = (id) => {
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
+  };
+  const handleArchive = () => {
+    console.log('archive');
+  };
   return (
     <div className="py-8">
       <h1>List Notes</h1>
       <hr />
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 pt-3">
-        {notes.length > 0 ? (
-          notes.map((note) => <CardNote key={note.id} {...note} />)
-        ) : (
-          <EmptyNotes />
-        )}
-      </div>
+      {notes.length > 0 ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 pt-3">
+          {notes.map((note) => (
+            <CardNote
+              key={note.id}
+              {...note}
+              handleDelete={handleDelete}
+              handleArchive={handleArchive}
+            />
+          ))}
+        </div>
+      ) : (
+        <EmptyNotes />
+      )}
     </div>
   );
 }
