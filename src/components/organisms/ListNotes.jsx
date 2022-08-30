b549c08c1c7b13d636e86fc1b289d29969314584
dennyzain <abbasdenny24@gmail.com>
2022-08-30 15:16:05 +0700

    fix: grid list notes on listNotes comp

--- src/components/organisms/ListNotes.jsx
+++ src/components/organisms/ListNotes.jsx
@@ -6,10 +6,10 @@ import { getInitialData } from '../../utils';
 export default function ListNotes() {
   const notes = getInitialData();
   return (
-    <div className="pt-8">
+    <div className="py-8">
       <h1>List Notes</h1>
       <hr />
-      <div className="grid grid-cols-3 gap-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 pt-3">
         {notes.length > 0 ? (
           notes.map((note) => <CardNote key={note.id} {...note} />)
         ) : (
