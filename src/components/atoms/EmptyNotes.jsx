1f079a8a12dbb4584f7fe687cc65bcbbac8fafef
dennyzain <abbasdenny24@gmail.com>
2022-08-31 11:30:25 +0700

    feat: archive list

--- src/components/atoms/EmptyNotes.jsx
+++ src/components/atoms/EmptyNotes.jsx
@@ -4,7 +4,7 @@ export default function EmptyNotes() {
   return (
     <div className="p-10">
       <img src="./empty.svg" alt="empty" width={400} className="m-auto" />
-      <p className="text-center p-6">Notes is empty...</p>
+      <p className="text-center p-6">List is empty...</p>
     </div>
   );
 }
--- src/components/organisms/ListNotes.jsx
+++ src/components/organisms/ListNotes.jsx
@@ -3,15 +3,30 @@ import EmptyNotes from '../atoms/EmptyNotes';
 import CardNote from '../molecules/CardNote';
 import { getInitialData } from '../../utils';
 
-export default function ListNotes() {
-  const [notes, setNotes] = useState(getInitialData());
-  const handleDelete = (id) => {
-    const newNotes = notes.filter((note) => note.id !== id);
-    setNotes(newNotes);
-  };
-  const handleArchive = () => {
-    console.log('archive');
-  };
+export default function ListNotes({ isArchiveList, archive, notes, handleArchive, handleDelete }) {
+  if (isArchiveList) {
+    return (
+      <div className="py-8">
+        <h1>List Archived Notes</h1>
+        <hr />
+        {archive.length > 0 ? (
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 pt-3">
+            {archive.map((note) => (
+              <CardNote
+                key={note.id}
+                handleArchive={handleArchive}
+                handleDelete={handleDelete}
+                {...note}
+              />
+            ))}
+          </div>
+        ) : (
+          <EmptyNotes />
+        )}
+      </div>
+    );
+  }
+
   return (
     <div className="py-8">
       <h1>List Notes</h1>
@@ -21,9 +36,9 @@ export default function ListNotes() {
           {notes.map((note) => (
             <CardNote
               key={note.id}
-              {...note}
               handleDelete={handleDelete}
               handleArchive={handleArchive}
+              {...note}
             />
           ))}
         </div>
