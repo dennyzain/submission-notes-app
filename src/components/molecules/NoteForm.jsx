9eb7fea552d4dbb7e307028d29c28651dd953f9f
dennyzain <abbasdenny24@gmail.com>
2022-09-01 11:45:59 +0700

    feat: add disabled button

--- src/components/molecules/NoteForm.jsx
+++ src/components/molecules/NoteForm.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { PlusSquare } from 'react-feather';
 
 export default function NoteForm({
-  title, content, setTitle, setContent, onSubmit,
+  title, content, setTitle, setContent, onSubmit, disabled,
 }) {
   return (
     <div className="m-auto grid grid-rows-[1fr_3fr_1fr] space-y-2">
@@ -22,8 +22,9 @@ export default function NoteForm({
       />
       <button
         onClick={onSubmit}
-        className="flex px-2 py-1 mx-auto justify-center border-1 w-1/2 border-2 border-black transition-all hover:shadow-[12px_12px_0px_0px_rgba(0,0,0,1)] "
+        className="flex px-2 py-1 mx-auto justify-center border-1 w-1/2 border-2 border-black transition-all hover:shadow-[12px_12px_0px_0px_rgba(0,0,0,1)] disabled:cursor-not-allowed disabled:opacity-50 disabled:shadow-none"
         type="button"
+        disabled={disabled}
       >
         Create New Note
         <PlusSquare className="pl-1" />
--- src/components/molecules/SearchForm.jsx
+++ src/components/molecules/SearchForm.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Search } from 'react-feather';
 
-export default function SearchForm({ handleSearch }) {
+export default function SearchForm({ handleSearch, disabled, onSearch }) {
   return (
     <div className="flex justify-center p-10">
       <input
@@ -9,7 +9,7 @@ export default function SearchForm({ handleSearch }) {
         className="text-base border-2 bg-white border-black p-1"
         onChange={handleSearch}
       />
-      <button type="button" className="py-1 px-3 border-2 border-l-0 border-black transition-all hover:shadow-[12px_12px_0px_0px_rgba(0,0,0,1)]">
+      <button type="button" onClick={onSearch} disabled={disabled} className="py-1 px-3 border-2 border-l-0 border-black transition-all hover:shadow-[12px_12px_0px_0px_rgba(0,0,0,1)] disabled:cursor-not-allowed disabled:opacity-50 disabled:shadow-none">
         <Search size={20} />
       </button>
     </div>
